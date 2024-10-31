08a3df0c250b397068db07bb9e2b36cb8daf1cda
Tashenica <85678340+Tashenica@users.noreply.github.com>
2024-11-01 00:25:45 +0200

    Adding API

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
-function generatePoem(event) {
-  event.preventDefault();
-
+function displayPoem(response) {
   new Typewriter("#poem", {
-    strings: "La tombe dit à la rose",
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
+}
+
+function generatePoem(event) {
+  event.preventDefault();
+
+  let apiKey = "da2510t155b3e135f01fffb4abebo4d6";
+  let prompt = "";
+  let context = "";
+  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiURL).then(displayPoem);
 }
 
 let poemFormElement = document.querySelector("#poem-generator-form");

0ddb38a51538f4a79048ffdd46eb569cbe7c77a8
Tashenica <85678340+Tashenica@users.noreply.github.com>
2024-11-04 00:48:20 +0200

    Updated AI

--- src/index.js
+++ src/index.js
@@ -10,12 +10,16 @@ function displayPoem(response) {
 function generatePoem(event) {
   event.preventDefault();
 
-  let instructionsInput = document.querySelector("user-instructions");
+  let instructionsInput = document.querySelector("#user-instructions");
   let apiKey = "da2510t155b3e135f01fffb4abebo4d6";
-  let prompt = `User instructions: Generate a Frenche poem about ${instructionsInput.value}`;
   let context =
-    "You are a romantic Poem expert and love to write short poems. Your mission is to generate a 4 line poem in basic HTML and separate each line with a <br />. Make sure to follow the user instructions.Do not include a title to the poem. Sign the poem with 'SheCodes AI' inside a <strong> element at the end of the poem and NOT at the beginning .";
+    "You are a romantic Poem expert and love to write short poems. You mission is to generate a 4 line poem in basic HTML and separate each line with a <br />. Make sure to follow the user instructions. Do not include a title to the poem. Sign the poem with 'SheCodes AI' inside a <strong> element at the end of the poem and NOT at the beginning";
+  let prompt = `User instructions: Generate a French poem about ${instructionsInput.value}`;
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  let poemElement = document.querySelector("#poem");
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = `<div class="generating">⏳ Generating a French poem about ${instructionsInput.value}</div>`;
 
   axios.get(apiURL).then(displayPoem);
 }
